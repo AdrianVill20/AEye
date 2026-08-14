12d2624e0c59d9022d620ae5f0431bd81d1c9f42
Christian Demetillo <dmtchrs@gmail.com>
2026-08-14 13:46:20 +0800

    up and down

--- integration/eye_gaze/main.py
+++ integration/eye_gaze/main.py
@@ -154,10 +154,10 @@ while True:
          vgaze = (vgaze_left + vgaze_right) / 2
 
          cv2.putText(frame, str(vgaze), (50, 200), font, 2, (0, 255, 0), 2)
-         if vgaze < 0.60:
+         if vgaze < 0.47:
              cv2.putText(frame, "looking up", (50, 250), font, 2, (0, 255, 0), 2)
              print('up')
-         elif 0.60 < vgaze < 0.75:
+         elif 0.47 < vgaze < 0.75:
              cv2.putText(frame, "looking straight", (50, 250), font, 2, (0, 255, 0), 2)
              print('straight')
          else:
